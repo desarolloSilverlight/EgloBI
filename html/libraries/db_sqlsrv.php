91a502f5853ac62cc1a8073139b1bea7cbabbe72
Leonardo Diaz <jldiaz9623@gmail.com>
2021-08-25 11:50:56 -0500

    Validacion sesion

--- html/libraries/db_sqlsrv.php
+++ html/libraries/db_sqlsrv.php
@@ -2,4 +2,9 @@
 
 $conexion = new PDO("sqlsrv:server=DESKTOP-G3BF7FS\SQLEXPRESS;database=EGLO", "sa", "Eglo1234");
 
+$consulta = $conexion->prepare("SELECT COUNT('CLIENTE') FROM EGLO.CLIENTE");
+$consulta->execute();
+$datos=$consulta->fetchAll(PDO::FETCH_ASSOC);
+var_dump($datos);
+
 ?>
